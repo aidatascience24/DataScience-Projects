5abe68282b49729ae0336efa5624649b477ee5ab
aidatascience24 <121124426+aidatascience24@users.noreply.github.com>
2023-04-24 22:34:07 +0530

    Update DataTypeValidationPrediction.py

--- creditCardDefaulters/DataTypeValidation_Insertion_Prediction/DataTypeValidationPrediction.py
+++ creditCardDefaulters/DataTypeValidation_Insertion_Prediction/DataTypeValidationPrediction.py
@@ -11,9 +11,6 @@ class dBOperation:
     """
           This class shall be used for handling all the SQL operations.
 
-          Written By: iNeuron Intelligence
-          Version: 1.0
-          Revisions: None
 
           """
 
@@ -31,10 +28,6 @@ class dBOperation:
                         Description: This method creates the database with the given name and if Database already exists then opens the connection to the DB.
                         Output: Connection to the DB
                         On Failure: Raise ConnectionError
-
-                         Written By: iNeuron Intelligence
-                        Version: 1.0
-                        Revisions: None
 
                         """
         try:
@@ -57,10 +50,6 @@ class dBOperation:
            Description: This method creates a table in the given database which will be used to insert the Good data after raw data validation.
            Output: None
            On Failure: Raise Exception
-
-            Written By: iNeuron Intelligence
-           Version: 1.0
-           Revisions: None
 
         """
         try:
@@ -109,10 +98,6 @@ class dBOperation:
                                        Output: None
                                        On Failure: Raise Exception
 
-                                        Written By: iNeuron Intelligence
-                                       Version: 1.0
-                                       Revisions: None
-
                 """
 
         conn = self.dataBaseConnection(Database)
@@ -158,10 +143,6 @@ class dBOperation:
                                                     above created .
                                        Output: None
                                        On Failure: Raise Exception
-
-                                        Written By: iNeuron Intelligence
-                                       Version: 1.0
-                                       Revisions: None
 
                 """
 
